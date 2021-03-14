a593498abffbcbe56f29aed4c30861a9ae83a10e
Lucas-Almeida-hub <lucasalmeidalvs@gmail.com>
2021-03-14 10:13:53 -0300

    atulizado modelo de analieses

--- tedencia.py
+++ tedencia.py
@@ -22,12 +22,11 @@ else:
 par = 'EURUSD'
 timeframe = 5
 
-velas = API.get_candles(par, (int(timeframe) * 60), 20,  time.time())
+velas = API.get_candles(par, (int(timeframe) * 60),1,  time.time())
 
-ultimo = round(velas[0]['close'], 4)
-primeiro = round(velas[-1]['close'], 4)
 
-diferenca = abs( round( ( (ultimo - primeiro) / primeiro ) * 100, 3) )
-tendencia = "CALL" if ultimo < primeiro and diferenca > 0.01 else "PUT" if ultimo > primeiro and diferenca > 0.01 else False
- 
+fechamento  = round(velas[0]['close'], 4) 
+abertura = round(velas[0]['close'], 4) 
+atual =
+
 print(tendencia)
